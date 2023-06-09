28a17f806a748ae0e13f786179d402d463663e4e
gros_jambon <julien@devcyclop.com>
2023-06-09 19:11:49 -0400

    Added normalization description

--- R/examples.R
+++ R/examples.R
@@ -7,7 +7,7 @@ normalize_0_1 <- function(x) {
 
 #' @title BIXI Data Class
 #'
-#' @description R6 class encapsulating the BIXI data
+#' @description R6 class encapsulating all BIXI dataframes
 #'
 #' @export
 BixiData <- R6::R6Class(
@@ -22,15 +22,15 @@ BixiData <- R6::R6Class(
 
         initialize = function() {
             self$departure_df <- BKTR::bixi_station_departures
-            # Normalize departure counts
+            # Normalize departure counts to [0, 1]
             max_val <- max(self$departure_df[, !c('location')], na.rm = TRUE)
             self$departure_df[, .SD / max_val, .SDcols = !c('location')]
 
-            # Normalize spatial features
+            # Normalize spatial features column wise to [0, 1]
             self$spatial_features_df <- BKTR::bixi_spatial_features
             self$spatial_features_df[, lapply(.SD, normalize_0_1), .SDcols = !c('location')]
 
-            # Normalize temporal features
+            # Normalize temporal features column wise to [0, 1]
             self$temporal_features_df <- BKTR::bixi_temporal_features
             self$temporal_features_df[, lapply(.SD, normalize_0_1), .SDcols = !c('time')]
 
